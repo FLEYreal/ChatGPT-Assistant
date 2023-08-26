48b1e463f45184390a6dcec795e936cb993d6592
FLEYreal <borisov.nikita.off@gmail.com>
2023-08-26 19:35:23 +0300

    Little design changes + STOP generating button added

--- locations/api.js
+++ locations/api.js
@@ -27,6 +27,7 @@ const sqlite3 = require('sqlite3').verbose()
 // Utils
 const axios = require('axios');
 const decoder = new TextDecoder();
+const controller = new AbortController();
 
 function apiApplication(config) {
 
@@ -83,89 +84,97 @@ function apiApplication(config) {
 
             // When prompt to chatGPT is sent
             socket.on('message_sent', async (data) => {
+                try {
 
-                // Get all variables
-                let gpt_response = '';
-                let { value, id } = data
+                    // Get all variables
+                    let gpt_response = '';
+                    let { value, id } = data
 
-                // If id is undefined
-                if (!id) {
-                    console.log('[\u001b[1;31mERROR\u001b[0m] : No ID / CUC-ID found')
-                    return res.status(404).json({ error: 'No ID / CUC-ID found' })
-                }
+                    // If id is undefined
+                    if (!id) {
+                        console.log('[\u001b[1;31mERROR\u001b[0m] : No ID / CUC-ID found')
+                        return res.status(404).json({ error: 'No ID / CUC-ID found' })
+                    }
 
-                // Get History of the conversation by ID
-                let history = await axios.post(`${process.env.API_IP}:${process.env.API_PORT}/chat/get-history`, {
-                    id: id
-                })
-                    .then(res => res.data.history)
-                    .catch(err => res.json({ error: err }))
-
-                if(history.error) return;
-
-                // Get newHistory, parse past one
-                let newHistory = JSON.parse(history)
-
-                // Push current prompt GPT
-                newHistory.push({
-                    role: 'user',
-                    content: value
-                })
-
-                // Make a POST request to the OpenAI API to get chat completions
-                const response = await fetch("https://api.openai.com/v1/chat/completions", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-                    },
-                    body: JSON.stringify({
-                        messages: newHistory,
-                        temperature: 0.1,
-                        model: config.gpt_version,
-                        max_tokens: config.max_tokens,
-                        stream: true,
+                    // Get History of the conversation by ID
+                    let history = await axios.post(`${process.env.API_IP}:${process.env.API_PORT}/chat/get-history`, {
+                        id: id
                     })
-                });
+                        .then(res => res.data.history)
+                        .catch(err => res.json({ error: err }))
 
-                // When chunk of the response gotten
-                for await (const chunk of response.body) {
-                    const decodedChunk = decoder.decode(chunk);
+                    if (history.error) return;
 
-                    // Clean up the data
-                    const lines = decodedChunk
-                        .split("\n")
-                        .map((line) => line.replace("data: ", ""))
-                        .filter((line) => line.length > 0)
-                        .filter((line) => line !== "[DONE]")
-                        .map((line) => JSON.parse(line));
+                    // Get newHistory, parse past one
+                    let newHistory = JSON.parse(history)
 
-                    // Destructuring!
-                    for (const line of lines) {
-                        const {
-                            choices: [
-                                {
-                                    delta: { content },
-                                },
-                            ],
-                        } = line;
+                    // Push current prompt GPT
+                    newHistory.push({
+                        role: 'user',
+                        content: value
+                    })
 
-                        if (content) {
-                            gpt_response += content;
-                            socket.emit('chunk', { content: content })
+                    // Make a POST request to the OpenAI API to get chat completions
+                    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+                        },
+                        body: JSON.stringify({
+                            messages: newHistory,
+                            temperature: 0.1,
+                            model: config.gpt_version,
+                            max_tokens: config.max_tokens,
+                            stream: true,
+                        }),
+
+                        // Use the AbortController's signal to allow aborting the request
+                        signal: controller.signal,
+                    });
+
+                    // When chunk of the response gotten
+                    for await (const chunk of response.body) {
+                        const decodedChunk = decoder.decode(chunk);
+
+                        // Clean up the data
+                        const lines = decodedChunk
+                            .split("\n")
+                            .map((line) => line.replace("data: ", ""))
+                            .filter((line) => line.length > 0)
+                            .filter((line) => line !== "[DONE]")
+                            .map((line) => JSON.parse(line));
+
+                        // Destructuring!
+                        for (const line of lines) {
+                            const {
+                                choices: [
+                                    {
+                                        delta: { content },
+                                    },
+                                ],
+                            } = line;
+
+                            if (content) {
+                                gpt_response += content;
+                                socket.emit('chunk', { content: content })
+                            }
                         }
                     }
+
+                    // Notify frontend when message is completed
+                    socket.emit('fully_received')
+
+                    // Save a prompt and response to history
+                    await axios.put(`${process.env.API_IP}:${process.env.API_PORT}/chat/save-history`, {
+                        id: id,
+                        prompt: value,
+                        gpt_response: gpt_response
+                    })
+
+                } catch (e) {
+                    console.error('[\u001b[1;31mERROR\u001b[0m] : (Might Be just a Request Abort, Nothing to worry)', e)
                 }
-
-                // Notify frontend when message is completed
-                socket.emit('fully_received')
-
-                // Save a prompt and response to history
-                await axios.put(`${process.env.API_IP}:${process.env.API_PORT}/chat/save-history`, {
-                    id: id,
-                    prompt: value,
-                    gpt_response: gpt_response
-                })
             })
 
             // When button to restart conversation is clicked
@@ -180,6 +189,15 @@ function apiApplication(config) {
                         .catch(err => {
                             console.log('[\u001b[1;31mERROR\u001b[0m] :', err)
                         })
+                }
+            })
+
+            socket.on('stop_generating', async () => {
+                try {
+                    // Stop streaming GPT response
+                    controller.abort();
+                } catch (e) {
+                    console.error('[\u001b[1;31mERROR\u001b[0m] :', error)
                 }
             })
         })
