d5f6272c1630ac7c6b8e0381a08de9d3201f5bcd
FLEYreal <borisov.nikita.off@gmail.com>
2023-08-27 22:31:52 +0300

    config.styles.js insides added

--- config.styles.js
+++ config.styles.js
@@ -3,5 +3,56 @@ module.exports = {
     // Width and Height of the Chat interface
     // PX - are pixels, you can also use % intead of "px"
     width: '100%',
-    height: '100svh'
+    height: '100svh',
+    background: '',
+    font_family: '',
+
+    header: {
+        color: {
+            background: '',
+            font: ''
+        },
+        shadow: ''
+    },
+
+    messages: {
+        user: {
+            color: {
+                background: '',
+                font: ''
+            }
+        },
+        gpt: {
+            color: {
+                background: '',
+                font: ''
+            }
+        }
+    },
+    input: {
+        color: {
+            background: '',
+            font: ''
+        },
+        shadow: '',
+        rounding: ''
+    },
+
+    buttons: {
+        color: {
+            background: '',
+            font: '',
+            border: ''
+        },
+        rounding: ''
+    },
+
+    error: {
+        color: {
+            background: '',
+            font: '',
+            border: ''
+        },
+        rounding: ''
+    }
 }
