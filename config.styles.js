12fb365d0b51cd80fce7d75d999391fe27df261f
FLEYreal <borisov.nikita.off@gmail.com>
2023-08-27 22:40:29 +0300

    Commented error wrapper code + Commented config.styles

--- config.styles.js
+++ config.styles.js
@@ -4,55 +4,108 @@ module.exports = {
     // PX - are pixels, you can also use % intead of "px"
     width: '100%',
     height: '100svh',
+
+    // Background of the page
     background: '',
+
+    // Font of the page
     font_family: '',
 
+    // Settings for the header
     header: {
+
+        // Settings for the color
         color: {
+            // Settings for color of the backround of the header
             background: '',
+
+            // Settings for color of the font of the header
             font: ''
         },
+
+        // Shadow settings, if you don't know CSS, use box-shadow CSS generators
         shadow: ''
     },
 
+    // Settings for the messages of GPT & User
     messages: {
+
         user: {
+
+            // Settings for the color
             color: {
+                // Settings for color of the backround of the user message
                 background: '',
+
+                // Settings for color of the font of the user message
                 font: ''
             }
         },
         gpt: {
+
+            // Settings for the color
             color: {
+                // Settings for color of the backround of the gpt message
                 background: '',
+
+                // Settings for color of the font of the gpt message
                 font: ''
             }
         }
     },
     input: {
+
+        // Settings for the color
         color: {
+            // Settings for color of the backround of the input
             background: '',
+
+            // Settings for color of the font of the input
             font: ''
         },
+
+        // Shadow settings, if you don't know CSS, use box-shadow CSS generators
         shadow: '',
+
+        // Rounding in pixels (px)
         rounding: ''
     },
 
+    // Settings of the buttons in action bar (buttons above input)
     buttons: {
+
+        // Settings for the color
         color: {
+            // Settings for color of the backround of the buttons
             background: '',
+
+            // Settings for color of the font of the buttons
             font: '',
+
+            // Settings for color of the border of the buttons
             border: ''
         },
+
+        // Rounding in pixels (px)
         rounding: ''
     },
 
+    // Settings for error wrapper that appears when error happened
     error: {
+
+        // Settings for the color
         color: {
+            // Settings for color of the backround of the error wrapper
             background: '',
+
+            // Settings for color of the font of the error wrapper
             font: '',
+
+            // Settings for color of the border of the error wrapper
             border: ''
         },
+
+        // Rounding in pixels (px)
         rounding: ''
     }
 }
