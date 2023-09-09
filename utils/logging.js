4666aa3670c4a43b2fc3273d02aed4571ffa6fcb
seylu <98249191+seyLu@users.noreply.github.com>
2023-09-09 16:35:58 +0800

    feat: add logging on and off

--- utils/logging.js
+++ utils/logging.js
@@ -3,6 +3,14 @@
 const chalk = require('chalk');
 
 class Logging {
+    static on(message) {
+        console.log(`[${chalk.bold.green('ON')}] : ${message}`);
+    }
+
+    static off(message) {
+        console.log(`[${chalk.bold.red('OFF')}] : ${message}`);
+    }
+
     static info(message) {
         console.log(`[${chalk.bold.cyan('INFO')}] : ${message}`);
     }
